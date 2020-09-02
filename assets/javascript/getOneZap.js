7e51228962541f1fe0d289fc5011f21c94de3c80
kimani kelly <kimanikelly@yahoo.com>
2020-09-02 12:44:49 -0400

    Added a clipboard icon next to the token address so users can copy the address on click

--- assets/javascript/getOneZap.js
+++ assets/javascript/getOneZap.js
@@ -1,13 +1,23 @@
 $(document).ready(() => {
 
+
     // $('.hide').hide();
+
+    $('.bi-clipboard').click(() => {
+        const copyAddress = $('#copy-address');
+
+        copyAddress.select();
+        const copiedAddress = document.execCommand('copy');
+        console.log(copiedAddress)
+    })
+
 
     // On click of the get-zap button this function starts the transaction to send users 1 ZAP
     $('#get-zap').click(async () => {
 
         // Gets the users meta mask address
         let accounts = await web3.eth.getAccounts();
-        
+
         // Creates a new instance of the Faucet.sol smart contract by connecting web3
         let faucetContract = await new web3.eth.Contract(faucetAbi, faucetAddress);
 
@@ -15,7 +25,7 @@ $(document).ready(() => {
             .buyZap()
             .send({ from: accounts[0], value: 1000000000000000 })
             .then((res) => {
-               
+
                 // Sets the 
                 $('#to').append(res.from);
                 $('#from').append(res.to);
