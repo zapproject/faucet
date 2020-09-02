e54a6e0f3f92f33c797ef6def37253f31afd923d
kimani kelly <kimanikelly@yahoo.com>
2020-09-02 11:27:16 -0400

    Set the favicon as the zap logo

--- assets/javascript/getOneZap.js
+++ assets/javascript/getOneZap.js
@@ -1,18 +1,22 @@
 $(document).ready(() => {
 
-    $('.hide').hide();
+    // $('.hide').hide();
 
+    // On click of the get-zap button this function starts the transaction to send users 1 ZAP
     $('#get-zap').click(async () => {
 
+        // Gets the users meta mask address
         let accounts = await web3.eth.getAccounts();
+        
+        // Creates a new instance of the Faucet.sol smart contract by connecting web3
         let faucetContract = await new web3.eth.Contract(faucetAbi, faucetAddress);
+
         faucetContract.methods
             .buyZap()
             .send({ from: accounts[0], value: 1000000000000000 })
-            // .then((receipt) => showReceipt(receipt)
             .then((res) => {
-                console.log(res.transactionHash)
-
+               
+                // Sets the 
                 $('#to').append(res.from);
                 $('#from').append(res.to);
                 $('#tx-hash').append(res.transactionHash)
